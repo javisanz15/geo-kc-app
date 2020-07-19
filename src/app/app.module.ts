61cba8a3586193d69575a77cf32300028a9e093e
ES\jsanzlopez <jsanzlopez@deloitte.es>
2020-07-19 23:34:20 +0200

    Add Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { HeaderComponent } from './header/pages/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
    declarations: [
@@ -21,6 +22,7 @@ import { HeaderComponent } from './header/pages/header.component';
       HttpClientModule,
       BrowserAnimationsModule,
       MatButtonModule,
+      MatToolbarModule,
    ],
    providers: [],
    bootstrap: [
